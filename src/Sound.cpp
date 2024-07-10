97490149781bb441f1894e1dfc1907231870af9a
headshot2017 <>
2024-07-10 16:44:27 -0400

    delete ADPCM buffer after uploading to SPU2
    also removed all SDL_mixer code

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -12,24 +12,6 @@ int* Sound::TimeStamps = nullptr;
 
 bool Sound::Init(int channels, bool enableFlag)
 {
-	/*
-	// change priority to make SDL audio thread run properly
-	int main_id = GetThreadId();
-	ChangeThreadPriority(main_id, 72);
-
-	if (SDL_Init(SDL_INIT_AUDIO) < 0)
-	{
-		printf("SDL_INIT_AUDIO failed: %s\n", SDL_GetError());
-		return false;
-	}
-
-	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 512) < 0)
-    {
-		printf("Mix_OpenAudio failed\n");
-        return false;    
-    }
-	*/
-
 	if (audsrv_init())
 		printf(" === Failed to init audsrv === \n");
 	if (audsrv_adpcm_init())
@@ -57,8 +39,6 @@ void Sound::Deactivate()
 
 void Sound::Close()
 {
-	//Mix_CloseAudio();
-	//SDL_Quit();
 	audsrv_quit();
 
 	delete[] TimeStamps;
@@ -68,7 +48,6 @@ void Sound::Close()
 void Sound::PlaySound(uint8_t* buf, int time, int size, int samplerate)
 {
 	if (!enabled_flag || !buf) return;
-	//int channel = Mix_PlayChannel(-1, (Mix_Chunk*)buf, 0);
 	int channel = audsrv_ch_play_adpcm(-1, (audsrv_adpcm_t*)buf);
 	if (channel >= 0)
 		TimeStamps[channel] = time;
@@ -115,19 +94,18 @@ uint8_t* Sound::LoadWaveFile(const std::string& lpName)
 	}
 
 	printf("loaded wav %s\n", lpName.c_str());
+	delete[] buffer;
 
-	/*Mix_Chunk* snd = Mix_LoadWAV(lpName.c_str());
-	if (!snd)
-		printf("Failed to load sound '%s'\n", lpName.c_str());
-	return (uint8_t*)snd;
-	*/
 	return (uint8_t*)sample;
 }
 
 void Sound::FreeSound(uint8_t* wave)
 {
-	//if (wave)
-		//Mix_FreeChunk((Mix_Chunk*)wave);
+	if (wave)
+	{
+		audsrv_free_adpcm((audsrv_adpcm_t*)wave);
+		delete wave;
+	}
 }
 
 void Sound::SetChannels(int channels)
